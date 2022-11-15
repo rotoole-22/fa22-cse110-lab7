bd327a82cc0edf59f7f4036cfe04e2f7002f98f6
rotoole-22 <zoner2019@gmail.com>
2022-11-14 19:53:21 -0800

    Quick fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -59,8 +59,8 @@ function initializeServiceWorker() {
   // STEPS B6 ONWARDS WILL BE IN /sw.js
   window.addEventListener('load', async () => {
     try {
-      const registration = await navigator.serviceWorker.register("/sw.js", {
-        scope: "/",
+      const registration = await navigator.serviceWorker.register("./sw.js", {
+        scope: "./",
       });
       if (registration.installing) {
         console.log("Service worker installing");
